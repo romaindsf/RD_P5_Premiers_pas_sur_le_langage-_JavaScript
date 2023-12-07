bf2611b6f532a32328f51f8daa671a728430bd2c
Romain Da Silva Faria <romaindsf@hotmail.fr>
2023-12-07 16:54:49 +0100

    text changes on click for both directions

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,9 @@ let leftArrow = document.querySelector(".arrow_left")
 let rightArrow = document.querySelector(".arrow_right")
 let slideNumber = 0
 let slidesLength = slides.length
-let slideShow = document.querySelector(".banner-img")
+let showImage = document.querySelector(".banner-img")
+let showText = document.querySelector("#banner p" )
+let textContent = ""
 
 //display dots equal to the numbers of slides
 let dots = document.querySelector(".dots")
@@ -55,16 +57,16 @@ function slidesLoop(slideNumber, slidesLength){
 function switchImages(slideNumber) {
 	switch(slideNumber){
 		case(0):
-		slideShow.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
+			showImage.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
 			break
 		case(1):
-			slideShow.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
+			showImage.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
 			break
 		case(2):
-			slideShow.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
+			showImage.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
 			break
 		case(3):
-		slideShow.setAttribute("src", "./assets/images/slideshow/slide4.png")
+			showImage.setAttribute("src", "./assets/images/slideshow/slide4.png")
 			break
 	}
 	return slideNumber
@@ -79,7 +81,23 @@ leftArrow.addEventListener("click", () => {
 	slideNumber = slidesLoop(slideNumber, slidesLength)
 	//display the right image
 	slideNumber = switchImages(slideNumber)
+	//change text content depending on slides
 
+	switch(slideNumber){
+		case(0):
+		textContent = `<p>Impressions tous formats <span>en boutique et en ligne</span></p>`
+			break
+		case(1):
+		textContent =  `<p>Tirages haute définition grand format <span>pour vos bureaux et events</span></p>`
+			break
+		case(2):
+		textContent =  `<p>Grand choix de couleurs <span>de CMJN aux pantones</span></p>`
+			break
+		case(3):
+		textContent =  `<p>Autocollants <span>avec découpe laser sur mesure</span></p>`
+			break
+	}
+	showText.innerHTML = textContent
 })
 
 rightArrow.addEventListener("click", () => {
@@ -88,4 +106,19 @@ rightArrow.addEventListener("click", () => {
 	slideNumber = slidesLoop(slideNumber, slidesLength)
 	slideNumber = switchImages(slideNumber)
 
+	switch(slideNumber){
+		case(0):
+		textContent = `<p>Impressions tous formats <span>en boutique et en ligne</span></p>`
+			break
+		case(1):
+		textContent =  `<p>Tirages haute définition grand format <span>pour vos bureaux et events</span></p>`
+			break
+		case(2):
+		textContent =  `<p>Grand choix de couleurs <span>de CMJN aux pantones</span></p>`
+			break
+		case(3):
+		textContent =  `<p>Autocollants <span>avec découpe laser sur mesure</span></p>`
+			break
+	}
+	showText.innerHTML = textContent
 })
