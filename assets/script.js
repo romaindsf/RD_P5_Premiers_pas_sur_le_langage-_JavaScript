c6f1c39bd9dd1428ead721562f61030c5af5561e
Romain Da Silva Faria <romaindsf@hotmail.fr>
2023-12-07 14:55:59 +0100

    eventlistener, number dots & first selected works

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,32 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//Variables globales déclarées
+let leftArrow = document.querySelector(".arrow_left")
+let rightArrow = document.querySelector(".arrow_right")
+let slideNumber = 0
+
+//display dots equal to the numbers of slides
+
+let dots = document.querySelector(".dots")
+for (let i = 0; i < slides.length; i++) {
+	let dot = document.createElement("div")
+	dot.classList.add("dot","dot_"+ i)
+	dots.appendChild(dot)
+}
+
+//differenciate the first selected dot
+let listDot = document.querySelectorAll(".dot")
+let selectedDot = listDot[slideNumber]
+selectedDot.classList.add("dot_selected")
+
+//event listener for both arrows
+
+leftArrow.addEventListener("click", () => {
+	console.log("cliqué sur flèche de gauche")
+})
+
+rightArrow.addEventListener("click", () => {
+	console.log("cliqué sur flèche de droite")
+})
